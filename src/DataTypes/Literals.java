120ece8b59955b21129801af4be7f82dec20a4ab
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-04-20 23:32:21 +0530

    completed total literals

--- src/DataTypes/Literals.java
+++ src/DataTypes/Literals.java
@@ -3,14 +3,14 @@ package DataTypes;
 public class Literals {
 
 	public static void main(String[] args) {
-		int a = 127;
-		int a1 = 0127;
-		int a2 = 0345;
-		int a3 = 0567;
-		int a4 = 0x123;
-		int a5 = 0x12ab;
-		int a6 = 0xDAD;
-		int a7 = 0Xbee;
+		int a =127;
+		int a1=0127;
+		int a2=0345;
+		int a3=0567;
+		int a4=0x123;
+		int a5=0x12ab;
+		int a6=0xDAD;
+		int a7=0Xbee;
 		System.out.println(a);
 		System.out.println(a1);
 		System.out.println(a2);
@@ -19,7 +19,7 @@ public class Literals {
 		System.out.println(a5);
 		System.out.println(a6);
 		System.out.println(a7);
-
+ 
 	}
 
 }
---
+++ src/DataTypes/Literals3.java
@@ -0,0 +1,23 @@
+package DataTypes;
+
+public class Literals3 {
+	static boolean bo = true;
+	static String name = "ratnakar";
+
+	public static void main(String[] args) {
+		char c = 'a';
+		char c1 = 99;
+		char c2 = '\u0034';
+
+		if (bo = true)
+			System.out.println(bo);
+		System.out.println(c);
+		System.out.println(c1);
+		System.out.println(c2);
+
+		System.out.println(name);
+		System.out.println();
+
+	}
+
+}
