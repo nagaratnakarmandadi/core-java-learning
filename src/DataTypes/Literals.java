a74b1eb3541e53af4ffa2e8ac25f3fbb3bfc6c16
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-04-20 23:02:59 +0530

    completed literals

---
+++ src/DataTypes/Literals.java
@@ -0,0 +1,25 @@
+package DataTypes;
+
+public class Literals {
+
+	public static void main(String[] args) {
+		int a = 127;
+		int a1 = 0127;
+		int a2 = 0345;
+		int a3 = 0567;
+		int a4 = 0x123;
+		int a5 = 0x12ab;
+		int a6 = 0xDAD;
+		int a7 = 0Xbee;
+		System.out.println(a);
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		System.out.println(a5);
+		System.out.println(a6);
+		System.out.println(a7);
+
+	}
+
+}
