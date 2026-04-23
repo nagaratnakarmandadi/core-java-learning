f7665048eb5a2233d101e181473c614b4d5afecd
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-04-23 23:15:34 +0530

    added bank details using string

---
+++ src/com/Methods/BankAccount.java
@@ -0,0 +1,31 @@
+package com.Methods;
+
+import java.util.Scanner;
+
+public class BankAccount {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter account holder name:");
+		String name = sc.nextLine();
+
+		System.out.println("Enter account number:");
+		long accNo = sc.nextLong();
+
+		System.out.println("Enter balance:");
+		double balance = sc.nextDouble();
+
+		System.out.println("Is account active? (true/false):");
+		boolean isActive = sc.nextBoolean();
+
+		System.out.println("\n--- Account Details ---");
+		System.out.println("Name: " + name);
+		System.out.println("Account No: " + accNo);
+		System.out.println("Balance: " + balance);
+		System.out.println("Active: " + isActive);
+
+		sc.close();
+	}
+}
