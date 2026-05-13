b278aa2fc5576a446aad0960b2feab308b8488bd
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-05-13 22:23:42 +0530

    implemented java program using assignment operator

---
+++ src/com/Operators/AssOperator.java
@@ -0,0 +1,21 @@
+package com.Operators;
+
+public class AssOperator {
+
+	public static void main(String[] args) {
+		int x = 10;
+		x += 100;
+		int y = 20;
+		y *= 20;
+		int c = 200;
+		c /= 20;
+		int d = 20;
+		d -= 20;
+
+		System.out.println("x value :" + x);
+		System.out.println("y value :" + y);
+		System.out.println("c value :" + c);
+		System.out.println("d value :" + d);
+	}
+
+}
