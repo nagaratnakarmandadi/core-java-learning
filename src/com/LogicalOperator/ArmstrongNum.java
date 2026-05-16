8945aad6a6ed8f489fb788fb62bd989726c1c134
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-05-16 23:08:19 +0530

    implemented armstrong number using loop

---
+++ src/com/LogicalOperator/ArmstrongNum.java
@@ -0,0 +1,30 @@
+package com.LogicalOperator;
+
+import java.util.Scanner;
+
+public class ArmstrongNum {
+
+	public static void main(String[] args) {
+		System.out.println("check armstrong or not ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number");
+		int num = sc.nextInt();
+		int temp = num;
+		int sum = 0;
+		int r = 0;
+		while (num != 0) {
+			r = num % 10;
+			sum = sum + (r * r * r);
+			num = num / 10;
+
+		}
+		if (temp == sum) {
+			System.out.println("armstrong");
+		} else {
+			System.out.println("not armstrong");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
