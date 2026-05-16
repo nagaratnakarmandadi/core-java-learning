6d9e75b8abd165fe1e4af34df8222456f00151b2
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-05-16 22:45:33 +0530

    implemented countdigit class using loops

--- src/com/LogicalOperator/CountDigit.java
+++ src/com/LogicalOperator/CountDigit.java
@@ -2,18 +2,20 @@ package com.LogicalOperator;
 
 import java.util.Scanner;
 
-public class Loop {
+public class CountDigit {
 
-	public static void main(String[] arg) {
+	public static void main(String[] args) {
+		System.out.println("count digit");
 		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a num");
 		int num = sc.nextInt();
 		int count = 0;
-		while (num > 0) {
+		while (num != 0) {
 			num = num / 10;
 			count++;
-
 		}
 		System.out.println(count);
+		// TODO Auto-generated method stub
 
 	}
 
