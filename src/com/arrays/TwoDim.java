41f4376f47e6b63fd170281c7236fbb48c42ecfd
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-05-20 23:13:00 +0530

    implemented java program using two dimensional

---
+++ src/com/arrays/TwoDim.java
@@ -0,0 +1,24 @@
+package com.arrays;
+
+public class TwoDim {
+
+	public static void main(String[] args) {
+
+		int[][] num = new int[3][3];
+
+		num[0][0] = 10;
+
+		num[1][1] = 100;
+		num[2][2] = 1000;
+
+		for (int i = 0; i < num.length; i++) {
+
+			for (int j = 0; j < num[i].length; j++) {
+
+				System.out.print(num[i][j] + " ");
+			}
+
+			System.out.println();
+		}
+	}
+}
