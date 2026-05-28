5edcac942f77604c9365f7493a729756e494b62f
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-05-28 10:58:58 +0530

    implemented butterflypattern

--- src/com/patterns/StarPattern4.java
+++ src/com/patterns/StarPattern4.java
@@ -3,19 +3,43 @@ package com.patterns;
 public class StarPattern4 {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 6; i++) {
+
+		int n = 6;
+
+		// upper part
+		for (int i = 1; i <= n; i++) {
+
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
-			System.out.println();
-		}
-		for (int i = 1; i <= 6; i++) {
-			for (int j = 6; j > i; j--) {
+
+			for (int j = 1; j <= 2 * (n - i); j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
+
 			System.out.println();
 		}
 
-	}
+		// lower part
+		for (int i = n - 1; i >= 1; i--) {
 
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+
+			for (int j = 1; j <= 2 * (n - i); j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+	}
 }
