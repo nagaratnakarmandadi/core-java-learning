cd86aa16a6329b0d0429ddf4239b6fa69d296546
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-05-28 09:34:52 +0530

    implemented star patterns

--- src/com/patterns/StarPatterns.java
+++ src/com/patterns/StarPatterns.java
@@ -1,18 +1,17 @@
 package com.patterns;
 
-public class StarPartterns {
+public class StarPatterns {
 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 6; i++) {
-			for (int j = 6; j > i; j--) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print(" ");
 			}
-			for (int k = 1; k <= i; k++) {
+			for (int k = 6; k > i; k--) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-
 	}
 
 }
