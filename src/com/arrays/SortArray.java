e0c50ab85df736e9a3630bea117fee12bb4c5b38
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-05-30 23:45:16 +0530

    implementedjava program to find sort array

---
+++ src/com/arrays/SortArray.java
@@ -0,0 +1,32 @@
+package com.arrays;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 5, 2, 8, 1, 9 };
+
+		for (int i = 0; i < arr.length; i++) {
+
+			for (int j = i + 1; j < arr.length; j++) {
+
+				if (arr[i] > arr[j]) {
+
+					int temp = arr[i];
+
+					arr[i] = arr[j];
+
+					arr[j] = temp;
+				}
+			}
+		}
+
+		System.out.println("Sorted Array:");
+
+		for (int i = 0; i < arr.length; i++) {
+
+			System.out.println(arr[i]);
+		}
+	}
+
+}
