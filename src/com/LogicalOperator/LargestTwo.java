92d6401848739b082c0b688bf37c61fafc4feffe
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-05-31 23:52:47 +0530

    implemented largest two

---
+++ src/com/LogicalOperator/LargestTwo.java
@@ -0,0 +1,19 @@
+package com.LogicalOperator;
+
+public class LargestTwo {
+
+    public static void main(String[] args) {
+
+        int a = 25;
+        int b = 40;
+
+        if(a > b) {
+
+            System.out.println(a + " is largest");
+        }
+        else {
+
+            System.out.println(b + " is largest");
+        }
+    }
+}
