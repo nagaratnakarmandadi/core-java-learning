4232e68585241c9878990840789e187ab19b1154
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-06-28 23:22:14 +0530

    implemented another program

---
+++ src/com/leetcode/CharacterFrequency1.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+import java.util.HashMap;
+
+public class CharacterFrequency1 {
+	public static void main(String[] args) {
+
+		String str = "programming";
+
+		HashMap<Character, Integer> map = new HashMap<>();
+
+		for (char ch : str.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0) + 1);
+		}
+
+		for (char key : map.keySet()) {
+			System.out.println(key + " : " + map.get(key));
+		}
+	}
+}
