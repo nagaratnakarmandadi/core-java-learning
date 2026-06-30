8290c627f410652cd836ca69805d3ebd7b768e45
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-06-30 22:39:55 +0530

    implemented missing number

---
+++ src/com/leetcode/MissingNumber1.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+public class MissingNumber1 {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 5, 6 };
+		int n = 6;
+
+		int expectedSum = n * (n + 1) / 2;
+		int actualSum = 0;
+
+		for (int num : arr) {
+			actualSum += num;
+		}
+
+		System.out.println("Missing Number = " + (expectedSum - actualSum));
+	}
+}
