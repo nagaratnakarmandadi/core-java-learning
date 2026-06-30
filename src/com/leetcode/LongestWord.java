9b625eca5d7b76920be363e4485a45d2ff1ea56c
nagaratnakarmandadi <nagaratnakarmandadi@gmail.com>
2026-06-30 22:43:25 +0530

    implemented longestword

---
+++ src/com/leetcode/LongestWord.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+public class LongestWord {
+	public static void main(String[] args) {
+		String sentence = "Java programming is very interesting";
+
+		String[] words = sentence.split(" ");
+		String longest = "";
+
+		for (String word : words) {
+			if (word.length() > longest.length()) {
+				longest = word;
+			}
+		}
+
+		System.out.println("Longest Word = " + longest);
+		System.out.println("Length = " + longest.length());
+	}
+}
